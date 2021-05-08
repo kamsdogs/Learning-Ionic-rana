d680c42977a1beec17022ae987a8db1ea9d8a68d
Kameron Hartman <47125766+kamsdogs@users.noreply.github.com>
2021-05-08 18:46:30 -0400

    Re-Opened
    Although the tutorial is outdated after reading up more I've understood how to convert some of the outdated things into modern working versions

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,7 +9,12 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+          }  
+        ]
       },
       {
         path: 'tab2',
