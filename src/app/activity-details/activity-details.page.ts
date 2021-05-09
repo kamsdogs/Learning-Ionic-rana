76b492ec3bc513a6743af1dbbfadd3789f4f63d4
Kameron Hartman <47125766+kamsdogs@users.noreply.github.com>
2021-05-09 18:02:24 -0400

    Implement Sharing
    Sharing also outdated from tutorial, implemented capacitor version.

--- src/app/activity-details/activity-details.page.ts
+++ src/app/activity-details/activity-details.page.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { ActivityVideoPage } from '../activity-video/activity-video.page';
 import { ActivityService } from '../activity.service';
 import { Activity } from '../types';
+import { Plugins } from '@capacitor/core';
+const { Share } = Plugins;
+
 
 @Component({
   selector: 'app-activity-details',
@@ -16,6 +19,7 @@ export class ActivityDetailsPage implements OnInit {
 
 
   constructor(
+    private _router: Router,
     private _modalController: ModalController,
     activityService: ActivityService,
     activatedRoute: ActivatedRoute) {
@@ -26,16 +30,26 @@ export class ActivityDetailsPage implements OnInit {
   ngOnInit() {
   }
 
-  async openModal() { 
+  async openModal() {
     const videoModal = await this._modalController.create({
       component: ActivityVideoPage
     });
 
-    return await this.activityDetail.subscribe((activity)=>{
+    return await this.activityDetail.subscribe((activity) => {
       videoModal.componentProps = {
         videoURL: activity.video_url
       };
       return videoModal.present();
+    });
+  }
+
+  async shareVideo() {
+    let urlFromPage = this._router.url;
+    let shareRet = await Share.share({
+      title: 'Look at this neat video!',
+      text: "Check out this video!",
+      url: urlFromPage,
+      dialogTitle: 'Share This Content'
     });
   }
 }
