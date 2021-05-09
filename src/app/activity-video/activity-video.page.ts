9e3b7fb203660c30eb7bdad4fbdbe65750e3a689
Kameron Hartman <47125766+kamsdogs@users.noreply.github.com>
2021-05-08 20:43:57 -0400

    Implement Modal Closing
    Implement Modal Closing

--- src/app/activity-video/activity-video.page.ts
+++ src/app/activity-video/activity-video.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-activity-video',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityVideoPage implements OnInit {
 
-  constructor() { }
+  constructor(private _modalController: ModalController) { }
 
   ngOnInit() {
   }
 
+  closeModal() {
+    this._modalController.dismiss();
+  }
 }
